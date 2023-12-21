953ad61663981d878963f9f98ad788e1480b3d8e
Srixx24 <7214@holbertonstudents.comgit config --global user.name Srixx24cd ..lsrungit config --global user.email 7214@holbertonstudents.com>
2023-12-21 12:13:15 -0800

    edits to push

--- execute.c
+++ execute.c
@@ -7,7 +7,7 @@
  */
 void executeline(const char *line, int line_num, stack_t **stack)
 {
-	char opcode[MAX_OPCODE_LENGTH] = {'\0'};
+	char opcode[MAX_OPCODE_LENGTH];
 	int value;
 	char *copy = strdup(line);
 	char *token = strtok(copy, " \t\n");
@@ -19,28 +19,36 @@ void executeline(const char *line, int line_num, stack_t **stack)
 	strncpy(opcode, token, MAX_OPCODE_LENGTH);
 	while (token != NULL)
 	{
-		if (sscanf((const char *)token, "%d", &value) == 1)
+
+		if (strcmp(opcode, "push") == 0)
 		{
-			if (strcmp(opcode, "push") == 0)
-				push(stack, line_num, &value);
-			else if (strcmp(opcode, "pall") == 0)
-				pall(stack);
-			else if (strcmp(opcode, "pint") == 0)
-				pint(stack, line_num);
-			else if (strcmp(opcode, "pop") == 0)
-				pop(stack, line_num);
-			else if (strcmp(opcode, "swap") == 0)
-				swap(stack, line_num);
-			else if (strcmp(opcode, "add") == 0)
-				add(stack, line_num);
-			else if (strcmp(opcode, "nop") == 0)
-				nop();
-			else
+			token = strtok(NULL, " \t\n");
+			if (token == NULL)
 			{
-				fprintf(stderr, "L%d: unknown instruction %s\n", line_num, opcode);
+				fprintf(stderr, "L%d: usage: push integer\n", line_num);
 				free(copy);
 				exit(EXIT_FAILURE);
 			}
+			sscanf((const char *)token, "%d", &value);
+			push(stack, line_num, &value);
+		}
+		else if (strcmp(opcode, "pall") == 0)
+			pall(stack);
+		else if (strcmp(opcode, "pint") == 0)
+			pint(stack, line_num);
+		else if (strcmp(opcode, "pop") == 0)
+			pop(stack, line_num);
+		else if (strcmp(opcode, "swap") == 0)
+			swap(stack, line_num);
+		else if (strcmp(opcode, "add") == 0)
+			add(stack, line_num);
+		else if (strcmp(opcode, "nop") == 0)
+			nop();
+		else
+		{
+			fprintf(stderr, "L%d: unknown instruction %s\n", line_num, opcode);
+			free(copy);
+			exit(EXIT_FAILURE);
 		}
 		token = strtok(NULL, " \t\n");
 	}
